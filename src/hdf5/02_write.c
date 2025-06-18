68a5babf2009d2f3d1bfbcee47bf0aad1c705c79
Kris Evers <krisevers14@gmail.com>
2025-06-18 13:16:48 +0200

    small folder re-organization

--- src/hdf5/02_write.c
+++ src/hdf5/02_write.c
@@ -4,7 +4,7 @@ int main(int argc, char** argv) {
     MPI_Init(&argc, &argv);
 
     hid_t plist_id, file_id;
-    if (hpcn_openFile("hello.h5", &plist_id, &file_id) != HPCN_SUCCESS) {
+    if (hpcn_openFile("data/hello.h5", &plist_id, &file_id) != HPCN_SUCCESS) {
         fprintf(stderr, "Opening file failed");
     }
 
