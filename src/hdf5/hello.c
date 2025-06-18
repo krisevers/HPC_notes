f4d12acf01f32c77a5f6cf772d97ac35f64e88ee
Kris Evers <krisevers14@gmail.com>
2025-06-18 07:29:54 +0200

    update hdf5 installation

--- src/hdf5/hello.c
+++ src/hdf5/hello.c
@@ -1,5 +1,5 @@
-#include <hdf5/serial/hdf5.h>
+#include <hdf5.h>
 
 int main(int argc, char** argv) {
-    
+
 }
--- src/install/install_hdf5.sh
+++ src/install/install_hdf5.sh
@@ -7,7 +7,7 @@
 #   2. download hdf5
 #   4. install hdf5
 #
-#   Usage: ./install_hdf5.sh [--parallel] [--prefix=/path/to/install]
+#   Usage: ./install_hdf5.sh [--parallel]
 # 
 #   @krisevers
 #
@@ -17,23 +17,15 @@
 sudo apt-get update
 sudo apt-get install -y build-essential openmpi-bin libopenmpi-dev wget
 
-# Download the latest HDF5 release (edit version as needed)
-HDF5_VERSION=1.14.3
-wget https://github.com/HDFGroup/hdf5/archive/refs/tags/hdf5-$HDF5_VERSION.tar.gz
-tar -xzvf hdf5-hdf5-$HDF5_VERSION.tar.gz
-cd hdf5-hdf5-$HDF5_VERSION
+# Download the latest HDF5 release
+git clone https://github.com/HDFGroup/hdf5.git
+cd hdf5
 
 # Configure with MPI support
-./configure --enable-parallel --prefix=/usr/local/hdf5-parallel CC=mpicc
-
-# Build and install
-make -j$(nproc)
+if [[ "$1" == "--parallel" ]]; then
+    cmake -DHDF5_ENABLE_PARALLEL=ON -DHDF5_BUILD_CPP_LIB=ON -DHDF5_ALLOW_UNSUPPORTED=ON ..
+else
+    cmake -DHDF5_BUILD_CPP_LIB=ON -DHDF5_ALLOW_UNSUPPORTED=ON ..
+fi
+cmake --build .
 sudo make install
-
-# Add HDF5 binaries and libraries to your environment (add this to your ~/.bashrc)
-export PATH=/usr/local/hdf5-parallel/bin:$PATH
-export LD_LIBRARY_PATH=/usr/local/hdf5-parallel/lib:$LD_LIBRARY_PATH
-
-# Verify installation
-mpicc -show
-h5pcc -showconfig
